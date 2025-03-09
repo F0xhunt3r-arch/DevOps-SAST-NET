70cb6f3027b7dfa7089251c1d0f66cb2120ee0e5
unknown <yeudielgb@gmail.com>
2025-03-09 15:29:19 -0600

    JENKINFILE_v7 commit

--- Models/LoginViewModel.cs
+++
@@ -1,6 +0,0 @@
-// filepath: C:\Users\Usuario\Desktop\DevSecOps_NET_CODE\DevSecOps_NET_CODE\Models\LoginViewModel.cs
-public class LoginViewModel
-{
-    public required string Username { get; set; }
-    public required string Password { get; set; }
-}
